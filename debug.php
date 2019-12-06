86fd8df9ea79b66279685927dbb28bd42effb96e
Nobel Dahal <iamtribulation@outlook.com>
2019-12-06 17:10:20 +0545

    Overriding styling, getting it to work on frontend for wp dd()s

--- debug.php
+++ debug.php
@@ -10,16 +10,32 @@
 if (!function_exists('dd')) {
     function dd(...$data)
     {
+	    ob_end_clean();
+	    ob_start();
+	    $resetStyling = "<style>
+					    .wiper{
+					      all: initial !important;
+					      * {
+					        all: unset !important; 
+					      }
+					    }
+					    </style>
+					  ";
+	    echo $resetStyling;
 	    ini_set("highlight.comment", "#969896; font-style: italic");
-	    ini_set("highlight.default", "#FFFFFF");
-	    ini_set("highlight.html", "#D16568");
-	    ini_set("highlight.keyword", "#7FA3BC; font-weight: bold");
-	    ini_set("highlight.string", "#F2C47E");
+	    ini_set("highlight.default", "#FFFFFF !important");
+	    ini_set("highlight.html", "#D16568 !important");
+	    ini_set("highlight.keyword", "#7FA3BC !important; font-weight: bold");
+	    ini_set("highlight.string", "#F2C47E !important");
 	    $output = highlight_string("<?php\n\n" . var_export($data, true), true);
-	    echo "<div style=\"background-color: #1C1E21; color:#7FA3BC; font-weight: bold padding: 1rem\">{$output}<br><hr>";
+	    echo "<span>
+    			<div 
+    			style=\"background-color: #1C1E21 !important; color:#7FA3BC; font-weight: bold padding: 1rem\">
+    			{$output}<br>
+    			<hr>";
 	    $output = highlight_string("<?php\n\n" . var_export(debug_backtrace(), true), true);
-	    echo "<div style=\"background-color: #1C1E21; color:#7FA3BC; font-weight: bold padding: 1rem\">{$output}<br>";
-	    echo "</div>";
-	    die();
+	    echo "<div style=\"background-color: #1C1E21 !important; color:#7FA3BC; font-weight: bold padding: 1rem\">{$output}<br>";
+	    echo "</div></span>";
+	    exit();
     }
 }
